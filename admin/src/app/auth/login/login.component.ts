3271b0b5b55bf4a94c7fb31d043688db4b352e41
diegoasousam <diego.araujo.s@gmail.com>
2025-02-25 22:25:53 -0300

    Ajustado Estilo Parcial das Telas

--- admin/src/app/auth/login/login.component.ts
+++ admin/src/app/auth/login/login.component.ts
@@ -4,11 +4,17 @@ import { HttpClient } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { environment } from '../../../environments/environment';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+
 
 @Component({
   standalone: true,
   selector: 'app-login',
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule,MatInputModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
